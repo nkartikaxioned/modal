7997f456e23eb091ec5c630548a7e8065a579e7f
nkartikaxioned <kartikn@axioned.com>
2023-10-11 15:28:55 +0530

    js functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,33 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
+const article = document.querySelectorAll('.content-container'),
+  button = document.querySelectorAll('.button'),
+  modal = document.querySelector('.modal'),
+  closebtn = document.querySelector('.btn'),
+  display = document.querySelector('.content'),
+  html = document.querySelector('html');
+ 
 
 
+article.forEach((art, index) => {
+  art.addEventListener('click', () => {
+   display.innerHTML=art.innerHTML
+    removeHidden()
+    html.classList.add('html-scroll')
+  })
+})
 
+function removeHidden() {
+  modal.classList.remove('hidden')
+}
 
+closebtn.addEventListener('click', () => {
+  modal.classList.add('hidden')
+  html.classList.remove('html-scroll')
+})
 
-
+display.addEventListener('click', () => {
+  modal.classList.add('hidden')
+  html.classList.remove('html-scroll')
+})
 
 
 
